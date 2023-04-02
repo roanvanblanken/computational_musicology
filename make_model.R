4868a4afaaedf126e44b4db7203327c593ad46b2
Roan <roanvanblanken.rvb@gmail.com>
2023-04-02 02:31:52 +0200

    NEW MUSIC FRIDAY WORKS WOO!

--- make_model.R
+++ make_model.R
@@ -4,6 +4,8 @@ library(ggdendro)
 library(heatmaply)
 library(spotifyr)
 library(compmus)
+library(rpart)
+library(rpart.plot)
 
 get_conf_mat <- function(fit) {
   outcome <- .get_tune_outcome_names(fit)
@@ -40,14 +42,14 @@ new_music_friday_sid <- get_playlist_audio_features("", "307LwouWuqsCBAwq9FYmYU"
 
 new_music_friday <-
   bind_rows(
-    songs_i_like |> mutate(playlist = "Songs I like"),
-    songs_i_dislike |> mutate(playlist = "Song I dislike")) |> 
+    new_music_friday_sil |> mutate(playlist = "Songs I like"),
+    new_music_friday_sid |> mutate(playlist = "Song I dislike")) |> 
   add_audio_analysis() 
 
 saveRDS(new_music_friday, file="data/Model (new_music_friday).Rda")
 
 new_music_friday_features <-
-  model |>  # For your portfolio, change this to the name of your corpus.
+  new_music_friday |>  # For your portfolio, change this to the name of your corpus.
   mutate(
     playlist = factor(playlist),
     segments = map2(segments, key, compmus_c_transpose),
@@ -100,6 +102,8 @@ model_features <-
 
 saveRDS(model_features, file="data/Model (model_features).Rda")
 
+model_features <- readRDS(file="data/Model (model_features).Rda")
+
 corpus_recipe <-
   recipe(
     playlist ~
@@ -116,8 +120,27 @@ corpus_recipe <-
     data = model_features           # Use the same name as the previous block.
   ) |>
   step_center(all_predictors()) |>
-  step_scale(all_predictors())      # Converts to z-scores.
-# step_range(all_predictors())    # Sets range to [0, 1].
+  step_scale(all_predictors()) |>   # Converts to z-scores.
+  step_range(all_predictors())      # Sets range to [0, 1].
+
+new_music_friday_recipe <-
+  recipe(
+    playlist ~
+      danceability +
+      energy +
+      loudness +
+      speechiness +
+      acousticness +
+      instrumentalness +
+      liveness +
+      valence +
+      tempo +
+      duration,
+    data = new_music_friday_features           # Use the same name as the previous block.
+  ) |>
+  step_center(all_predictors()) |>
+  step_scale(all_predictors()) |>   # Converts to z-scores.
+  step_range(all_predictors())      # Sets range to [0, 1].
 
 corpus_cv <- model_features |> vfold_cv(5)
 
@@ -164,6 +187,35 @@ corpus_decision_tree |> get_conf_mat() |> autoplot(type = "heatmap")
 
 # Get precision and recall
 corpus_decision_tree |> get_pr()
+
+# Fit the decision tree model for new music friday
+nmf_decision_tree <- 
+  workflow() |> 
+  add_recipe(new_music_friday_recipe) |> 
+  add_model(decision_tree_model) |> 
+  fit(data = new_music_friday_features)
+
+# Get predictions from the model
+nmf_predictions <- predict(nmf_decision_tree, new_music_friday_features) 
+
+# Create a tibble with the true and predicted labels
+nmf_pred <- tibble(
+  truth = new_music_friday_features$playlist, 
+  estimate = nmf_predictions$.pred_class
+)
+
+# Compute the confusion matrix
+nmf_conf_mat <- conf_mat(nmf_pred, truth = truth, estimate = estimate)
+
+# Plot confusion matrix as mosaic plot
+nmf_conf_mat |> autoplot(type = "mosaic")
+
+# Plot confusion matrix as heatmap
+nmf_conf_mat |> autoplot(type = "heatmap")
+
+overview <- bind_cols(new_music_friday_features %>% select(track.name), nmf_pred)
+
+
 
 # Fit the decision tree model without cross-validation
 corpus_decision_tree_no_cv <- 
